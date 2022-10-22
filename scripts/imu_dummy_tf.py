eb7ddf4e95cd8f0245007ff2d68bc66277d6a803
sasilva1998 <sasilva1998@gmail.com>
2022-10-22 12:42:31 +0100

    chore: frame params

--- scripts/imu_dummy_tf.py
+++ scripts/imu_dummy_tf.py
@@ -20,14 +20,18 @@ if __name__ == "__main__":
     p_offset = rospy.get_param("~p_offset", 0)
     y_offset = rospy.get_param("~y_offset", 0)
 
+    # FRAMES TO BE PUBLISHED
+    parent_frame = rospy.get_param("~parent_frame")
+    child_frame = rospy.get_param("~child_frame")
+
     q_rot = quaternion_from_euler(r_offset, p_offset, y_offset)
 
     broadcaster = tf2_ros.StaticTransformBroadcaster()
     static_transformStamped = geometry_msgs.msg.TransformStamped()
 
     static_transformStamped.header.stamp = rospy.Time.now()
-    static_transformStamped.header.frame_id = "a1_gazebo/imu_link"
-    static_transformStamped.child_frame_id = "a1_gazebo/imu_link_dummy"
+    static_transformStamped.header.frame_id = parent_frame
+    static_transformStamped.child_frame_id = child_frame
 
     static_transformStamped.transform.translation.x = x_offset
     static_transformStamped.transform.translation.y = y_offset
